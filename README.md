22c4e835e5047362c247c884efb2da6307c34c88
JongMany <blackberry1114@naver.com>
2024-04-01 20:49:03 +0900

    prerender 적용

--- README.md
+++ README.md
@@ -1,30 +1,17 @@
-# React + TypeScript + Vite
+# 프로젝트 개요
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- React 프로젝트에서 SEO 대응을 위한 최소 작업 대응
 
-Currently, two official plugins are available:
+## 개발환경 세팅
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- fork 후, npm install 해주시면
 
-## Expanding the ESLint configuration
+## SEO 스코어 분석
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+- 스코어 분석에 사용한 서비스: (링크 적기, 예: https://www.seobility.net/en/seocheck/)
 
-- Configure the top-level `parserOptions` property like this:
+### 개선점
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+- 스코어 개선을 위해 필요한 작업 1
+- 스코어 개선을 위해 필요한 내용 2
+- 스코어 개선을 위해 필요한 내용 3
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+// import Prerenderer from "@prerenderer/renderer-puppeteer";
+// import Prerenderer from "@prerenderer/prerenderer";
+// import JSDOMRenderer from "@prerenderer/renderer-jsdom";
+import prerender from "@prerenderer/rollup-plugin";
+
+// const prerenderer = new Prerenderer({
+//   // Required - The path to the app to prerender. Should have an index.html and any other needed assets.
+//   staticDir: path.join(__dirname, "app"),
+//   // The plugin that actually renders the page.
+//   renderer: new JSDOMRenderer(),
+//   postProcess(renderedRoute) {
+//     // Replace all http with https urls and localhost to your site url
+//     renderedRoute.html = renderedRoute.html
+//       .replace(/http:/i, "https:")
+//       .replace(
+//         /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
+//         process.env.CI_ENVIRONMENT_URL || ""
+//       );
+//   },
+// });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    prerender({
+      routes: ["/", "/counter", "/otherpage/1", "/otherpage/2", "/otherpage/3"],
+      renderer: "@prerenderer/renderer-puppeteer",
+      server: {
+        port: 3000,
+        host: "localhost",
+      },
+      rendererOptions: {
+        maxConcurrentRoutes: 1,
+        renderAfterTime: 500,
+      },
+      postProcess(renderedRoute) {
+        renderedRoute.html = renderedRoute.html
+          .replace(/http:/i, "https:")
+          .replace(
+            /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
+            "https://seo-optimization-test.netlify.app/"
+          );
+      },
+    }),
+  ],
+});
